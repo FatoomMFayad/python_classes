72c62513a129e2d75da406a12ab3389f2f1e5a09
ffayad <ffayad@ptcdb.edu.ps>
2025-01-13 14:39:24 +0200

    perimeter and area functions

--- Rectangle.py
+++ Rectangle.py
@@ -1,3 +1,10 @@
 class Rectangle:
     width = 0
     length = 0
+
+    def perimeter(self):
+       return (self.length + self.width) * 2
+
+    def area(self):
+        return (self.length * self.width)
+
