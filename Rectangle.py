6842a321b9a0e20fd290b506c15ab7fe052f280c
ffayad <ffayad@ptcdb.edu.ps>
2025-01-13 14:43:52 +0200

    display and create rectangle instance

--- Rectangle.py
+++ Rectangle.py
@@ -8,3 +8,14 @@ class Rectangle:
     def area(self):
         return (self.length * self.width)
 
+    def display(self):
+        return print(f"Rectangle:\nwidth = {self.width},\nlength = {self.length}, \nperimeter = {self.perimeter()},\narea = {self.area()}")
+
+
+r1 = Rectangle()
+r1.width = 10
+r1.length = 20
+r1.perimeter()
+r1.area()
+r1.display()
+
