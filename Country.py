23b8df4f28c1a09b4fa2fbe27dda33feca825e4a
ffayad <ffayad@ptcdb.edu.ps>
2025-01-14 13:20:06 +0200

    Country class

---
+++ Country.py
@@ -0,0 +1,26 @@
+class Country:
+    name = ''
+    population = 0
+    capital = ''
+    outcome = 0
+    income = 0
+
+c1 = Country()
+c1.name = 'Palestine'
+c1.capital = 'Jerusalem'
+c1.population = 13000000
+c1.outcome = 70000
+c1.income = 10000
+
+c2 = Country()
+c2.name = 'Yemen'
+c2.capital = 'Sanaa'
+c2.population = 8000000
+c2.outcome = 60000
+c2.income = 10000
+
+countries = [c1, c2]
+for country in countries:
+    print(country.name)
+
+
